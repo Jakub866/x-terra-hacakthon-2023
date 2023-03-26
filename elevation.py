43584849a87d6cd4a2bb4f7624c47d3b0e33928c
felaxd <44652616+felaxd@users.noreply.github.com>
2023-03-26 10:33:45 +0200

    Added elevation script

---
+++ elevation.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+HFiles_dir = "hfiles/"
+width = 1476
+
+plot = []
+current_line = []
+last_known_elevation = 0
+files_len = len(os.listdir(HFiles_dir))
+filenames = [f"hfile{i}.txt" for i in range(files_len)]
+for filename in filenames:
+    file_path = f"{HFiles_dir}/{filename}"
+    with open(file_path, "r") as file:
+        _info = json.loads(file.readline().replace("'", '"')).get("results", [])
+        for pixel in _info:
+            last_known_elevation = pixel.get("elevation", last_known_elevation) or last_known_elevation
+            current_line.append(str(int(last_known_elevation)))
+            if len(current_line) == width:
+                plot.append(current_line)
+                last_known_elevation = current_line[0]
+                current_line = []
+
+out = open("elevation.txt", "w")
+out.write("\n".join([", ".join(l) for l in plot]))
